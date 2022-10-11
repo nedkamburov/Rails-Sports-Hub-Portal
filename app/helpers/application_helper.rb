d296ec9a8e02d61a391097b828ea0b39f1fa2a31
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-10-11 11:54:53 +0300

    add side panel and panel links generation helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,23 +10,58 @@ module ApplicationHelper
     end
   end
 
-  def nav_items
+  def side_panel_items
     [
       {
         url: root_path,
         title: 'Home'
+      },
+      {
+        url: 'path-to-be-added',
+        title: 'Team hub'
+      },
+      {
+        url: 'path-to-be-added',
+        title: 'Lifestyle'
+      },
+      {
+        url: 'path-to-be-added',
+        title: 'Dealbook'
+      },
+      {
+        url: 'path-to-be-added',
+        title: 'Video'
       }
     ]
   end
 
-  def nav_helper style, tag_type
-    nav_links = ''
+  # Todo: replace this mock with actual Sports when they are added
+  def mock_sports
+    [
+      {
+        url: 'path-to-be-added',
+        title: 'NBA'
+      },
+      {
+        url: 'path-to-be-added',
+        title: 'NFL'
+      },
+      {
+        url: 'path-to-be-added',
+        title: 'NASCAR'
+      }
+    ]
+  end
 
-    nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+  def side_panel_helper style, tag_type
+    panel_links = ''
+
+    panel_items = side_panel_items.insert(1, *mock_sports)
+    panel_items.each do |item|
+      panel_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
-    nav_links.html_safe
+    panel_links.html_safe
   end
 
   def active? path
