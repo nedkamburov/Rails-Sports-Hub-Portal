ef6ebbe2defdee04283421fa3b66e30e81a0d2d0
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-10-17 10:22:19 +0200

    add category model that can reference itself so we get subcategories too

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_091609) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_081418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.bigint "parent_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_category_id"], name: "index_categories_on_parent_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_091609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "categories", column: "parent_category_id"
 end
