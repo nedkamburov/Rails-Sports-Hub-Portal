81cd38902b1a52141ed208e84c3f599ff78c176b
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-10-17 11:26:54 +0200

    fix the foreign key declaration in the model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
+  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_category_id", :dependent => :destroy
   belongs_to :parent_category, :class_name => "Category", :optional => true
 end
