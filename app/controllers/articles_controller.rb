76df697eca048dcc72843c87298a76a688fb69ad
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-02 17:28:55 +0200

    half-way styling of the article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show ]
   def index
   end
 
   def show
-    @category = Category.friendly.find(params[:id])
+    @category = Category.find(@article.category_id)
+    @team = Team.find(@article.team_id)
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
 
