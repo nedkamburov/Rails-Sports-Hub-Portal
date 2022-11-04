d8967ba5c3ee7a60ed605d6b7c6d4282bc753d4b
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-04 11:55:03 +0200

    add custom delete confirmation modal with Turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,16 @@ import "@fortawesome/fontawesome-free"
 Turbo.session.drive = false
 import "trix"
 import "@rails/actiontext"
+
+Turbo.setConfirmMethod((recordType, element) => {
+    let dialog = document.getElementById("turbo-confirm")
+    if (recordType) dialog.querySelectorAll(".record-type")
+        .forEach(el => el.textContent = recordType  )
+    dialog.showModal()
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue == "confirm")
+        }, { once: true })
+    })
+})
