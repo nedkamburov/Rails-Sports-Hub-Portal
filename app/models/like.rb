f8288abecb4927f043cc266b7736721c5d84781b
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-08 12:22:29 +0200

    add like model to keep track of likes on the comments

---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+  validates :content, :user_id, :article_id, presence: true
+
+  belongs_to :user
+  belongs_to :article
+  belongs_to :parent, class_name: 'Comment', optional: true
+  has_many :comments, foreign_key: :parent_id
+  has_many :likes, as: :likeable
+end
---
+++ db/migrate/20221108092948_create_likes.rb
@@ -0,0 +1,14 @@
+class CreatePolymorphicLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :likeable, null: false, foreign_key: true
+      t.string :likeable_type, null: false
+
+      t.timestamps
+    end
+
+    add_index :likes, [:user_id, :likeable_id, :likeable_type], unique: true
+    add_index :likes, [:likeable_id, :likeable_type]
+  end
+end
