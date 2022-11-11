a076828e47d5b3a8e51c85d823eff3dad2d76c27
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-11 11:50:37 +0200

    add custom layout for devise actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :admin do
     root :to => "pages#home"
