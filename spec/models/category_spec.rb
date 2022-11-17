303df60c6b5d8fb4d722422d2ad3b1ab6766b403
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-17 12:06:09 +0200

    update the categories models tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,75 +3,80 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
    let(:category) { FactoryBot.build(:category) }
 
+   describe 'generally' do
+     it 'must be valid' do
+       expect(category).to be_valid
+     end
+   end
+
    describe 'title' do
       it 'must be present' do
-        expect(category).to be_valid
-        expect(category.title).to_not be_empty
+        expect(category.title).to_not be_nil
       end
     end
 
     describe 'position' do
       it 'must be present' do
-        expect(category).to be_valid
-        expect(category.position).to_not be nil
+
+        expect(category.position).to_not be_nil
       end
     end
 
    describe 'read_only' do
      it 'must be present' do
-       expect(category).to be_valid
-       expect(category.read_only).to_not be nil
+       expect(category.read_only).to_not be_nil
      end
 
      it 'must be "false" by default' do
-       expect(category).to be_valid
-       expect(category.read_only).to be false
+       expect(category.read_only).to be_falsy
      end
    end
 
    describe 'category_type' do
      it 'must be present' do
-       expect(category).to be_valid
-       expect(category.category_type).to_not be nil
+       expect(category.category_type).to_not be_nil
      end
 
      it 'must be "articles" by default' do
-       expect(category).to be_valid
        expect(category.category_type).to eq 'articles'
      end
    end
 
    describe 'slug' do
+     before do
+       category.save!
+     end
+
      it 'must be present' do
-       expect(category).to be_valid
-       expect(category.slug).to_not be nil
+       expect(category.slug).to_not be_nil
      end
 
      it 'must be similar to the title' do
-       expect(category).to be_valid
        expect(category.slug).to include(*category.title.split.map { |s| s.downcase })
      end
    end
 
     describe 'subcategories' do
-      category = FactoryBot.create(:category)
-      category.subcategories.create(title: 'Subcategory')
-
-      it 'must accept subcategories' do
-        expect(category.subcategories).to_not be_empty
+      before(:all) do
+        @category = FactoryBot.create(:category)
+        @category.subcategories.create(title: 'Subcategory')
+        @subcategory = @category.subcategories.first
+        @subcategory.teams.create(title: 'Team')
+        @team = @subcategory.teams.first
       end
 
-      subcategory = category.subcategories.first
-      subcategory.teams.create(title: 'Team')
+      it 'must be able to access subcategories' do
+        expect(@category.subcategories).to_not be_empty
+      end
+
       it 'must be able to access the subcategory properties' do
-        expect(subcategory.title).to eq 'Subcategory'
-        expect(subcategory.teams).to_not be_empty
+        expect(@subcategory.title).to eq 'Subcategory'
+        expect(@subcategory.teams).to_not be_empty
       end
 
       it 'must be able to access the teams properties' do
-        team = subcategory.teams.first
-        expect(team).to be_valid
-        expect(team.title).to_not be_empty
+        expect(@team).to be_valid
+        expect(@team.title).to_not be_empty
       end
     end
 
