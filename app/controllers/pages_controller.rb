65af174a0179c36dbffe0a65cafa7926fbe422f9
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-22 11:20:08 +0200

    add styling for the front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+
+  def home
+    @main_articles = Article.where(is_part_of_main_articles: true).order("RANDOM()")
+    @breakdown_articles = Article.where(is_part_of_breakdown: true).order("RANDOM()")
+    @photo_of_the_day = PhotoOfTheDay.last
+    @most_commented = Article.limit(3).order(created_at: :desc)
+    @most_popular = Article.limit(3).order(created_at: :asc)
+  end
+
   def create
   end
 end
