23a87ba2fccb60a1a969a4fa4e33ac708c140fe3
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2022-11-25 16:52:04 +0200

    add database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'devise'
 require_relative 'support/controller_macros'
+require 'support/database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,8 +39,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
-
+  config.use_transactional_fixtures = false # it is handled by database_cleaner
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
