a45c18640d4311974ea8230640b6c769a0694a35
nedkamburov <nedislav.kamburov@gmail.com>
2024-02-24 16:05:06 +0200

    fix build

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,5 @@
 //= link_directory ../stylesheets .scss
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
 // = link application.css
 // = link devise.css
